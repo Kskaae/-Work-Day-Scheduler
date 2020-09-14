2e894b1db9d6858a63b431f1e7beef162dbded1d
Kristopher Kaae <kskaae@gmail.com>
2020-09-13 21:08:53 -0700

    added times

---
+++ Script.js
@@ -0,0 +1,34 @@
+//GIVEN I am using a daily planner to create a schedule
+const day
+const date
+const time
+const saveTheInformation
+const dailySchedule = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+};
+//$().grab workday information 
+    
+//WHEN I open the planner
+//THEN the current day is displayed at the top of the calendar
+let dayDisp = currentDay
+//WHEN I scroll down
+function scroll()
+//THEN I am presented with timeblocks for standard business hours
+//How do I add timeblocks?
+//WHEN I view the timeblocks for that day
+//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+//WHEN I click into a timeblock
+//THEN I can enter an event
+//WHEN I click the save button for that timeblock
+//THEN the text for that event is saved in local storage
+//WHEN I refresh the page
+//THEN the saved events persist
